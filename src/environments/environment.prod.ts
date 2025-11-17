b85eb1c3b62b7580280563942956210de21032e2
Shweta-Khair <77388880+Shweta-Khair@users.noreply.github.com>
2025-11-17 12:09:28 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   movieServiceUrl: '/api/movies',  // Proxied through Nginx
-  reviewServiceUrl: '/api/review', // Changed from reviews to review
+  reviewServiceUrl: '/api/reviews', // Changed 
   enableTracing: true
 };
